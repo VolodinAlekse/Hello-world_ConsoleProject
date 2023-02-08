41f5207a944673be436fda04811458748c5ce371
VolodinAlekse <volodin.8989@mail.ru>
2023-02-08 16:36:04 +0300

    Example012_Methods155line

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -132,4 +132,24 @@ void PrintArray(int[] array)
     Console.WriteLine();
 }
 
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
 PrintArray(arr);
