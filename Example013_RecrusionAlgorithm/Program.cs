553529a8089dde729c72e197e19b34c8b1f707fb
VolodinAlekse <volodin.8989@mail.ru>
2023-02-13 13:30:02 +0300

    Fibonacci

--- Example013_RecrusionAlgorithm/Program.cs
+++ Example013_RecrusionAlgorithm/Program.cs
@@ -102,14 +102,33 @@
 
 
 
-double Factorial(int n)
+// double Factorial(int n)
+// {
+//     //1!=1
+//     //0!=1
+//     if (n == 1) return 1;
+//     else return n * Factorial(n - 1);
+// }
+// for (int i = 1; i < 40; i++)
+// {
+//     Console.WriteLine($"{i}! = {Factorial(i)}");
+// }
+
+
+
+
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+
+double fibonacci(int n)
 {
-    //1!=1
-    //0!=1
-    if (n == 1) return 1;
-    else return n * Factorial(n - 1);
+    if (n == 1 || n == 2) return 1;
+    else return fibonacci(n - 1) + fibonacci(n - 2);
 }
-for (int i = 1; i < 40; i++)
+
+for (int i = 1; i < 50; i++)
 {
-    Console.WriteLine($"{i}! = {Factorial(i)}");
+    Console.WriteLine($"f({i}) = {fibonacci(i)}");
 }
